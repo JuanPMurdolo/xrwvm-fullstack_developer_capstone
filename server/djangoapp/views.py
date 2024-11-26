5e75b992fc49e582d4393edc6830178f1358976a
JuanPMurdolo <jpmurdolo@gmail.com>
2024-11-26 12:58:30 -0300

    Dealerships

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -88,8 +88,10 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# def get_dealerships(request):
-# ...
+def get_dealerships(request):
+    context = {}
+    return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
