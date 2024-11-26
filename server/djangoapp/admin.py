7dec51d7b3978b6ef873c499c2b8e216d564d0fb
JuanPMurdolo <jpmurdolo@gmail.com>
2024-11-26 11:40:55 -0300

    Fixing admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -17,5 +17,5 @@ class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
 # Register models here
-admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarMakeAdmin)
 
