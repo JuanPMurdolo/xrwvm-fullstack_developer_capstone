301619d59f589de30c0b80c193eb72dac177466b
JuanPMurdolo <jpmurdolo@gmail.com>
2024-11-27 00:42:20 -0300

    lintin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,23 +1,18 @@
-# from django.contrib import admin
-# from .models import related models
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# Inline class for CarModel
-class CarModelInline(admin.TabularInline):  # Use StackedInline if you prefer a stacked layout
+class CarModelInline(admin.TabularInline):
     model = CarModel
-    extra = 1  # Number of empty rows to display for adding new models
-    fields = ('name', 'type', 'year', 'dealer_id')  # Fields to show in the inline admin
-    show_change_link = True  # Add a link to edit the related CarModel entry
+    extra = 1  
+    fields = ('name', 'type', 'year', 'dealer_id')  
+    show_change_link = True
 
-# Register the CarMake model with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'established')
     search_fields = ('name',)
     list_filter = ('established',)
-    inlines = [CarModelInline]  # Attach the inline
+    inlines = [CarModelInline]
 
-# Register the CarModel model with customizations
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
     search_fields = ('name', 'car_make__name')
@@ -26,6 +21,5 @@ class CarModelAdmin(admin.ModelAdmin):
     raw_id_fields = ('car_make',)
     list_per_page = 20
 
-# Register models to the admin site
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,18 @@
-# Uncomment the following imports before adding the Model code
 
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
     name = models.CharField(null=False, max_length=30, default='Car Make')
     description = models.CharField(null=False, max_length=1000, default='Description')
     established = models.DateField(null=True)
+    
     def __str__(self):
         return "Name: " + self.name + "," + \
                "Description: " + self.description + "," + \
                "Established: " + str(self.established)
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models)
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealer_id = models.IntegerField(null=False)
@@ -32,13 +25,13 @@ class CarModel(models.Model):
     ]
     type = models.CharField(null=False, max_length=30, choices=CAR_TYPES, default='Sedan')
     year = models.DateField(null=True)
+    
     def __str__(self):
         return "Car Make: " + self.car_make.name + "," + \
                "Dealer ID: " + str(self.dealer_id) + "," + \
                "Name: " + self.name + "," + \
                "Type: " + self.type + "," + \
                "Year: " + str(self.year)
-    
 
 class CarDealer:
     def __init__(self, address, city, full_name, id, lat, long, short_name, zip):
@@ -50,18 +43,7 @@ class CarDealer:
         self.long = long
         self.short_name = short_name
         self.zip = zip
+    
     def __str__(self):
         return "Dealer name: " + self.full_name
-
-    def to_dict(self):
-        return {
-            "address": self.address,
-            "city": self.city,
-            "full_name": self.full_name,
-            "id": self.id,
-            "lat": self.lat,
-            "long": self.long,
-            "short_name": self.short_name,
-            "zip": self.zip,
-        }
    
