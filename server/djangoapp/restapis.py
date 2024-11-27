aa49f860028b3ae12f5db8542d4b0ee24796960b
JuanPMurdolo <jpmurdolo@gmail.com>
2024-11-26 23:15:12 -0300

    Rollbaclk

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,63 +1,50 @@
 # Uncomment the imports below before you add the function code
-# import requests
-import os
 import requests
-import json
+import os
 from dotenv import load_dotenv
-from.models import CarDealer
-from requests.auth import HTTPBasicAuth
-
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+def get_request(endpoint, **kwargs):
+    params = ""
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-# def post_review(data_dict):
-# Add code for posting review
-def get_request(url, **kwargs):
-    print(kwargs)
-    print("GET from {} ".format(url))
+    request_url = backend_url + endpoint + "?" + params
+
+    print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs)
-        status_code = response.status_code
-        print("With status {} ".format(status_code))
-        json_data = json.loads(response.text)
-        return json_data
-    except:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
-    return 0
 
-def get_dealers_from_cf(**kwargs):
-    results = []
-    url = "https://jpmurdolo-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/fetchDealers"
-    json_result = get_request(url)
-    if json_result:
-        dealers = json_result
-        for dealer in dealers:
-            dealer_doc = dealer
-            dealer_obj = CarDealer(
-                address=dealer_doc["address"],
-                city=dealer_doc["city"],
-                full_name=dealer_doc["full_name"],
-                id=dealer_doc["id"],
-                lat=dealer_doc["lat"],
-                long=dealer_doc["long"],
-                short_name=dealer_doc["short_name"],
-                zip=dealer_doc["zip"],
-            )
-            results.append(dealer_obj)
-    return [dealer.to_dict() for dealer in results]  # Convert objects to dicts
+
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url + "/analyze/" + text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+
+
+def post_review(data_dict):
+    request_url = backend_url + "/insert_review"
+    try:
+        response = requests.post(request_url, json=data_dict)
+        print(response.json())
+        return response.json()
+    except Exception:
+        print("Network exception occurred")
